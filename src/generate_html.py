72e1bc9bfddcb548b156ac0ed4753463b5697949
Stuart <30924578+thedaedalus@users.noreply.github.com>
2026-02-13 14:06:15 +0800

    Add new content pages and refactor HTML generator

--- src/generate_html.py
+++ src/generate_html.py
@@ -27,44 +27,32 @@ def list_files(*path):
 
 
 def generate_page(from_path, template_path, dest_path):
-    html_content = {}
-    markdown_files = []
-    html_output = []
-    print(f"Generating page from {from_path} to {dest_path} using {template_path} ")
-    if not from_path.exists() or not template_path.exists():
-        raise Exception(f"Make sure that {from_path} and {dest_path} exists")
-    source_files = list_files(from_path)
+    print(f"* {from_path} {template_path} -> {dest_path}")
+    with open(from_path, mode="r", encoding="utf-8") as markdown_file:
+        markdown_content = markdown_file.read()
+    markdown_file.close()
+    with open(template_path, mode="r", encoding="utf-8") as template_file:
+        template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+    with open(dest_path, mode="w", encoding="utf-8") as to_file:
+        to_file.write(template)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    dest_file = ""
+    source_files = list_files(dir_path_content)
     for file in source_files:
-        if ".md" in file.lower():
-            try:
-                with open(file, mode="r", encoding="utf-8") as markdown_file:
-                    markdown_files.append(markdown_file.read())
-
-            except Exception as e:
-                raise RuntimeError(f"ERROR: Failed to load {markdown_file}: {e} file might be corrupted")
-
-    try:
-        with template_path.open(mode="r", encoding="utf-8") as template_file:
-            template_html = template_file.read()
-    except Exception as e:
-        raise RuntimeError(f"ERROR: Failed to load {template_file}: {e} file might be corrupted")
-
-    for i in range(len(markdown_files)):
-        if html_content.get(markdown_files[i]) is None:
-            title = extract_title(markdown_files[i])
-        if not title:
-            raise Exception("Markdown file does not contain a Title")
-        # print(title)
-        html_content.setdefault(i, {})
-        html_content[i]["title"] = title
-        html_node = markdown_to_html_node(markdown_files[i])
-        html_content[i]["html"] = html_node.to_html()
-
-    for file, content in html_content.items():
-        output_html = re.sub(r"{{ Title }}", content["title"], template_html)
-        output_html = re.sub(r"{{ Content }}", content["html"], output_html)
-        html_output.append(output_html)
-    if not Path(dest_path).is_dir():
-        os.mkdir(dest_path)
-    with open(f"{Path(dest_path)}/index.html", mode="w", encoding="utf-8") as output:
-        output.write(html_output[0])
+        dest_file = file.replace("content", dest_dir_path)
+        dest_file = dest_file.replace("md", "html")
+        generate_page(file, template_path, dest_file)
--- src/main.py
+++ src/main.py
@@ -2,17 +2,17 @@ import os
 from pathlib import Path
 
 from copystatic import copy_recursive
-from generate_html import extract_title, generate_page, list_files
+from generate_html import extract_title, generate_page, generate_pages_recursive, list_files
 
 
 def main():
     dest_path = "public"
     src_path = "static"
     copy_recursive(src_path, dest_path)
-    from_path = Path("content")
-    template_path = Path("template.html")
-
-    generate_page(from_path, template_path, dest_path)
+    from_path = "content"
+    template_path = "template.html"
+    dest_file = "public"
+    generate_pages_recursive(from_path, template_path, dest_path)
 
 
 if __name__ == "__main__":
